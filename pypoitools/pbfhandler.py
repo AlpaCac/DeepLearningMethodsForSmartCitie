d414d229afde8f16c3b8af022ebef188e05b80f2
AlpaCa <2604307870@qq.com>
2024-07-17 14:26:50 +0800

    feat: 获取区域内poi类型

--- pypoitools/pbfhandler.py
+++ pypoitools/pbfhandler.py
@@ -8,11 +8,32 @@ warnings.simplefilter(action='ignore', category=FutureWarning)  #解决FutureWar
 
 
 class PbfHandler:
-    def __init__(self, pbf, output_dir):
+    def __init__(self, pbf, output_dir, polygon):
         self.pbf = pbf
         self.output_dir = output_dir
-        self.osm = pyrosm.OSM(pbf)  #创建osm对象
+        self.osm = pyrosm.OSM(pbf, bounding_box=polygon)  #创建osm对象
         self.map = folium.Map(location=self.getpos(), zoom_start=10)  # 创建map对象
+
+        self.poi_type_list = list()
+        self.cal_poi_types()
+    '''
+    'amenity', 'building',
+    'drinking_water', 'fast_food', 'internet_access', 'landuse', 'office',
+    'parking', 'post_office', 'social_facility', 'source', 'start_date',
+    'wikipedia', 'bicycle', 'books', 'clothes', 'organic', 'religion',
+    'second_hand', 'shop', 'trade', 'water', 'attraction', 'information',
+    'museum', 'tourism', 'geometry', 'osm_type', 'building:levels',
+    'fountain', 'school', 'theatre', 'wholesale', 'zoo'
+    '''
+    def cal_poi_types(self):
+        pois = self.get_pois()
+        for index, row in pois.iterrows():
+            if row['amenity'] not in self.poi_type_list and row['amenity'] != None:
+                self.poi_type_list.append(row['amenity'])
+            if row['shop'] not in self.poi_type_list and row['shop'] != None:
+                self.poi_type_list.append(row['shop'])
+            if row['tourism'] not in self.poi_type_list and row['tourism'] != None:
+                self.poi_type_list.append(row['tourism'])
 
     # TODO: 计算中心点
     def getpos(self):
@@ -36,6 +57,13 @@ class PbfHandler:
         gdf = gpd.GeoDataFrame(pois)
         folium.GeoJson(gdf).add_to(self.map)  # 将geo数据加入map
 
-    def set_bounding(self):
-        polygon = Polygon(((38, 115), (40, 117)))
-        self.osm = self.osm = pyrosm.OSM(self.pbf, bounding_box=polygon)  #创建osm对象
+    def set_bounding(self, polygon):
+        self.osm = pyrosm.OSM(self.pbf, bounding_box=polygon)  #创建osm对象
+        self.poi_type_list = list()
+        self.cal_poi_types()
+
+    def get_pois(self):
+        return self.osm.get_pois()
+
+    def get_poi_type_list(self):
+        return self.poi_type_list
