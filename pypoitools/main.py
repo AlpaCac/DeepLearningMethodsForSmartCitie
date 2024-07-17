3591c3fafdf371b0760c68272c41f97b8f492639
AlpaCa <2604307870@qq.com>
2024-07-17 20:55:31 +0800

    feat: 完成区域道路长度和区域道路密度

--- pypoitools/main.py
+++ pypoitools/main.py
@@ -1,4 +1,13 @@
 import pandas as pd
+import pyrosm
+import matplotlib.pyplot as plt
+from area import area
+import geopandas as gpd
+from pyproj import CRS
+from shapely.ops import unary_union
+from pyproj import Geod
+from shapely.geometry import Point, LineString, Polygon, mapping
+
 from pbfhandler import PbfHandler
 from shapely.geometry.polygon import Polygon
 
@@ -9,9 +18,31 @@ pbf_dir = "F:\\items\\生产实习20240705\\data\\pbf\\Beijing.osm.pbf"  #.osm.p
 output_dir = "F:\\items\\生产实习20240705\\data"  #.geojson数据位置
 
 polygon = Polygon([(116.36,39.92), (116.37,39.92), (116.37,39.93), (116.36,39.93), (116.36,39.92)])
-pbfhandler = PbfHandler(pbf_dir, output_dir, polygon=None)
+pbfhandler = PbfHandler(pbf_dir, output_dir, polygon=polygon)
+
+
 # print(pbfhandler.get_pois())
 # print(pbfhandler.get_pois().count())
-print(pbfhandler.get_poi_type_list())
+# print(pbfhandler.get_poi_num())
+# print(pbfhandler.get_poi_type_list())
+
+# streets = pbfhandler.get_streets()
+# print(streets.columns)
+# print(pbfhandler.get_street_length())
+# pbfhandler.get_street_density()
+
+# boundaries = pbfhandler.get_boundaries()
+# gdf = gpd.GeoDataFrame(index=[0], geometry=[boundaries])
+# gdf.crs = {'init': 'epsg:4326'} # WGS84
+# fig, ax = plt.subplots(figsize=(10, 10))
+# gdf.plot(ax=ax, facecolor='red', edgecolor='black')
+# polygon1 = Polygon([(115.2,39.28), (117.3,39.28), (117.3,41.05), (115.2,41.05), (115.2,39.28)])
+# obj = mapping(boundaries)
+# area = area(obj)/1e6
+# print(area)
+
+print(pbfhandler.get_street_density())
 # pbfhandler.add_pois_to_map()
-# pbfhandler.save_map()
+pbfhandler.add_streets_to_map()
+pbfhandler.save_map()
+plt.show()
--- pypoitools/pbfhandler.py
+++ pypoitools/pbfhandler.py
@@ -1,8 +1,11 @@
 import warnings
 import folium
 import pyrosm
+from area import area
 import geopandas as gpd
 from shapely import Polygon
+from shapely.geometry import mapping
+from shapely.ops import unary_union
 
 warnings.simplefilter(action='ignore', category=FutureWarning)  #解决FutureWarning问题
 
@@ -35,6 +38,31 @@ class PbfHandler:
             if row['tourism'] not in self.poi_type_list and row['tourism'] != None:
                 self.poi_type_list.append(row['tourism'])
 
+    def get_poi_num(self):
+        pois = self.get_pois()
+        return len(pois)
+
+    def get_street_length(self):
+        streets = self.get_streets()
+        res = 0
+        for index, row in streets.iterrows():
+            res+=row['length']
+        return res
+
+    def get_street_density(self):
+        length = self.get_street_length() # 米
+        obj = mapping(self.get_boundaries())
+        s = area(obj)/1e6 # 平方公里
+        return length/s
+
+    def get_boundaries(self):
+        if self.osm.bounding_box != None:
+            return self.osm.bounding_box
+        boundaries = self.osm.get_boundaries(boundary_type="all")
+        all_polygons = [geom for geom in boundaries['geometry']]
+        merged_polygons = unary_union(all_polygons)
+        return merged_polygons
+
     # TODO: 计算中心点
     def getpos(self):
         return [39.9042, 116.4074]
@@ -64,6 +92,9 @@ class PbfHandler:
 
     def get_pois(self):
         return self.osm.get_pois()
+
+    def get_streets(self):
+        return self.osm.get_network(network_type="driving")
 
     def get_poi_type_list(self):
         return self.poi_type_list
